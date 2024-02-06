36e4787f287073d06c524ae760c830567e3c0fab
Yusri <muhd.yusri@rhbgroup.com>
2024-02-06 17:13:44 +0800

    change bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{ backgroundColor: "#FAFAFA" }} className={inter.className}>
+      <body style={{ backgroundColor: "#fff" }} className={inter.className}>
         {children}
       </body>
     </html>
